ca0dd2259f95bfbe9c790cc910fa772f3ae3abbb
chapaev72 <40496276+chapaev72@users.noreply.github.com>
2018-06-22 19:45:49 +0500

    Add files via upload

---
+++ Program2.pas
@@ -0,0 +1,14 @@
+﻿program roadcalc;
+label z,x,y,h;
+var a,b:int64;
+c:char;
+begin
+h:
+writeln('Введите среднюю расчётную скорость:');
+readln(a);
+y:
+writeln('Введите предпологаемый путь:');
+readln(b);
+writeln('Время в пути: ',b/a);
+goto y;
+end.

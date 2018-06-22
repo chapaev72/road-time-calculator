d5f335765e870c09fee775c4282a24bb3ad946e3
chapaev72 <40496276+chapaev72@users.noreply.github.com>
2018-06-22 19:46:57 +0500

    Update Program1.pas

--- Program1.pas
+++ Program1.pas
@@ -1,4 +1,4 @@
-﻿program roadcalc;
+program roadcalclite;
 label z,x,y,h;
 var a,b:int64;
 c:char;
@@ -10,14 +10,5 @@ y:
 writeln('Введите предпологаемый путь:');
 readln(b);
 writeln('Время в пути: ',b/a);
-writeln('Повторить? Д/Н');
-readln(c);
-if c='Д' then goto z
-else goto x;
-z:
-writeln('скорость та же? Д/Н');
-readln(c);
-if c='Д' then goto y
-else goto h;
-x:
+goto y;
 end.
